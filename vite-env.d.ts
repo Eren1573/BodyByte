13b5500b443c905fcb4bf52508748562995b6d9e
B.Sai Naga Sowri <isowri4ku@gmail.com>
2026-03-17 21:49:29 +0530

    Update vite-env.d.ts

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -3,8 +3,6 @@
 interface ImportMetaEnv {
   readonly VITE_API_KEY_1: string;
   readonly VITE_API_KEY_2: string;
-  readonly VITE_API_KEY_3: string;
-  readonly VITE_API_KEY_4: string;
 }
 
 interface ImportMeta {
